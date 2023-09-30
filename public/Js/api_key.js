96a10534e6afb8e6fb2eacf407344a7df2202742
Jean-Yves2 <sarakajean1@gmail.com>
2023-09-30 23:26:04 +0200

    fix-logic

--- public/Js/api_key.js
+++
@@ -1,3 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const API_KEY='f0d274191e07b607933ec4ad047b2138';
-
--- public/Js/base.js
+++ public/Js/base.js
@@ -1,73 +1,55 @@
 /* eslint-disable no-undef */
-"use Script";
+"use strict";
 
-document.addEventListener("DOMContentLoaded",()=>{
-
+document.addEventListener("DOMContentLoaded", () => {
   let form = document.getElementById("name_form");
-  let nom_de_ville = document.querySelector('#nom_de_ville');
 
-  form.addEventListener("submit",function(event){
-
+  form.addEventListener("submit", async function (event) {
     event.preventDefault();
-    meteo(nom_de_ville);
+    const nom_de_ville = document.querySelector("#nom_de_ville").value;
+
+    try {
+      const response = await fetch(`/weather?cityName=${nom_de_ville}`);
+      const data = await response.json();
+
+      let ville = data.location.name;
+      let temperature = data.current.temperature;
+      let vitesse_du_vent = data.current.wind_speed;
+
+      let humidity = data.current.humidity;
+      let pressure = data.current.pressure;
+      let countryName = data.location.country;
+
+      const dateOrigin = dayjs(data.location.localtime, "YYYY-MM-DD HH:mm");
+      const formattedDate = dateOrigin.format("dddd, D MMMM YYYY");
+
+      const img = document.querySelector("img");
+      img.src = data.current.weather_icons[0];
+
+      let is_day = data.current.is_day;
+      const backgroundPage = document.getElementById("background");
+
+      backgroundPage.classList.remove("night", "day", "default_page");
+
+      if (is_day == "no") {
+        backgroundPage.classList.add("night");
+      } else {
+        backgroundPage.classList.add("day");
+      }
+
+      document.getElementById("name_ville").innerHTML = ville;
+      document.getElementById("temp").innerHTML = temperature + "°C";
+      document.getElementById("vitesse").innerHTML = vitesse_du_vent;
+
+      document.getElementById("humidity").innerHTML = humidity + "%";
+      document.getElementById("country").innerHTML = countryName;
+      document.getElementById("pressure").innerHTML = pressure;
+
+      document.getElementById("date").innerHTML = formattedDate;
+    } catch (error) {
+      console.error(error);
+    }
+
     form.reset();
-
   });
-
-
-  function meteo(nom_de_ville){
-    fetch(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${nom_de_ville.value}`)
-
-      .then(response => response.json())
-      .then(response=>{
-        console.log(response);
-
-        let ville =response['location']['name'];
-        let temperature=response['current']['temperature'];
-        let vitesse_du_vent =response['current']['wind_speed'];
-
-        let humidity =response['current']['humidity'];
-        let pressure = response['current']['pressure'];
-        let countryName= response['location']['country'];
-
-        //time with dayjs
-        let time = response['location']['localtime'];
-        console.log(time);
-
-        const dateOrigin = dayjs(time, "YYYY-MM-DD HH:mm");
-        const formattedDate = dateOrigin.format("dddd, D MMMM YYYY");
-        console.log(formattedDate);
-
-        const img=document.querySelector("img");
-        img.src = response['current']['weather_icons'];
-
-        let is_day =response['current']['is_day'];
-        backgroundPage = document.getElementById('background');
-
-        backgroundPage.classList.remove("night", "day", "default_page");
-        console.log(is_day);
-        if(is_day=='no'){
-          backgroundPage.classList.add('night');
-        }else{
-          backgroundPage.classList.add('day');
-        }
-
-
-        document.getElementById("name_ville").innerHTML =ville;
-        document.getElementById("temp").innerHTML =temperature+"°C";
-        document.getElementById("vitesse").innerHTML =vitesse_du_vent;
-
-        document.getElementById("humidity").innerHTML =humidity+"%";
-        document.getElementById("country").innerHTML = countryName;
-        document.getElementById("pressure").innerHTML = pressure;
-
-        document.getElementById("date").innerHTML = formattedDate;
-
-      });
-  }
-}
-
-
-
-
-);
+});
